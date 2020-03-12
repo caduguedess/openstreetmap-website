ffd5d987c0f74f227653629a4eaf27b8017f3f7a
Carlos <caduguedess@gmail.com>
2020-03-12 16:58:23 -0300

    resize badges

--- app/helpers/ranking_helper.rb
+++ app/helpers/ranking_helper.rb
@@ -2,15 +2,15 @@ module RankingHelper
   def badge_image(name)
     case name
     when 'changesets10' 
-      image_tag("https://i.imgur.com/Ufz5sp0.png")
+      image_tag("https://i.imgur.com/rMMnEGY.png")
     when 'changesets30' 
-      image_tag("https://i.imgur.com/2sszFua.png")
+      image_tag("https://i.imgur.com/HSaX1s7.png")
     when 'changesets80' 
-      image_tag("https://i.imgur.com/o9dANZm.png")
+      image_tag("https://i.imgur.com/7z47I1I.png")
     when 'friends'
-      image_tag("https://i.imgur.com/vQwGOTR.png")
+      image_tag("https://i.imgur.com/eiauHfp.png")
     when 'posts'
-      image_tag("https://i.imgur.com/yHhwcMF.png")
+      image_tag("https://i.imgur.com/q5EIO2o.png")
     end
   end
 end
